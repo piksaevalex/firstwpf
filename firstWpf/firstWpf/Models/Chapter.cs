95e31ca0b4cc8b3a4d68fb68365c7e2924679cbf
piksaevalex <34372869+piksaevalex@users.noreply.github.com>
2018-06-10 21:35:40 +0300

    low view

--- firstWpf/firstWpf/Models/Chapter.cs
+++ firstWpf/firstWpf/Models/Chapter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -32,60 +33,73 @@ namespace firstWpf.Models
             XDocument xdoc = XDocument.Parse(text);
             
 
-            foreach (XElement Chapterss in xdoc.Root.Elements("Chapters"))
+            foreach (XElement chapterss in xdoc.Root.Elements("Chapters"))
             {
-                foreach (XElement Chapter in Chapterss.Elements("Chapter"))
+                foreach (XElement chapter in chapterss.Elements("Chapter"))
                 {
                     int Count = 0;
-                    XAttribute Chapter1 = Chapter.Attributes("Caption").First();
+                    XAttribute chapter1 = chapter.Attributes("Caption").First();
                     //Chapters newChapter;
                     //Chapter0.Add(newChapter = new Chapters() { Caption = Chapter1.Value.ToString() });
 
-                    foreach (XElement Position in Chapter.Elements("Position"))
+                    foreach (XElement position in chapter.Elements("Position"))
                     {
-                        XAttribute Caption = Position.Attributes("Caption").First();
-                        XAttribute Code = Position.Attributes("Code").First();
-                        XAttribute units = Position.Attributes("Units").First();
+                        XAttribute caption = position.Attributes("Caption").First();
+                        XAttribute code = position.Attributes("Code").First();
+                        XAttribute units = position.Attributes("Units").First();
                         Count++;
-                        XElement Quantity = Position.Elements("Quantity").First();
-                        XAttribute Quantity1 = Quantity.Attributes("Fx").First();
+                        XElement quantity = position.Elements("Quantity").First();
+                        XAttribute quantity1 = quantity.Attributes("Fx").First();
+                        string str = quantity1.Value.ToString();
+                        str = str.Replace(",", ".");
+                        var result = new DataTable().Compute(str, null);
+                        //var result = (double)new DataTable().Compute(quantity1.Value.ToString(), null);
                         //Position0.Add(new Positions(Count, Code.Value.ToString(), Caption.Value.ToString(), units.Value.ToString(), Quantity1.Value.ToString()));
 
 
 
 
-                        foreach (XElement Resources in Position.Elements("Resources"))
+                        foreach (XElement resources in position.Elements("Resources"))
                         {
-                            foreach (XElement Tzm in Resources.Elements("Tzm"))
+                            foreach (XElement tzm in resources.Elements("Tzm"))
                             {
-                                XAttribute Tzm1 = Tzm.Attributes("Caption").First();
+                                XAttribute tzm1 = tzm.Attributes("Caption").First();
+                                XAttribute tzmMchCode = tzm.Attributes("Code").First();
+                                XAttribute tzmMchQuantity = tzm.Attributes("Quantity").First();
+                                
                                 //TzmMch0.Add(new TzmMch() { Caption = Tzm1.Value.ToString() });
                                 this.Add(new Chapter()
                                 {
-                                    Chaptername = Chapter1.Value.ToString(),
+                                    Chaptername = chapter1.Value.ToString(),
                                     PositionNumber = Count,
-                                    PositionCode = Code.Value.ToString(),
-                                    Positionname = Caption.Value.ToString(),
+                                    PositionCode = code.Value.ToString(),
+                                    Positionname = caption.Value.ToString(),
                                     PositionUnits = units.Value.ToString(),
-                                    PositionQuantity = Quantity1.Value.ToString(),
+                                    PositionQuantity = result.ToString(),
 
-                                    TzmMchname = Tzm1.Value.ToString()
+                                    TzmMchname = tzm1.Value.ToString(),
+                                    TzmMchCode = tzmMchCode.Value.ToString(),
+                                    TzmMchQuantity = tzmMchQuantity.Value.ToString()
                                 });
                             }
-                            foreach (XElement Mch in Resources.Elements("Mch"))
+                            foreach (XElement mch in resources.Elements("Mch"))
                             {
-                                XAttribute Mch1 = Mch.Attributes("Caption").First();
+                                XAttribute mch1 = mch.Attributes("Caption").First();
+                                XAttribute tzmMchCode = mch.Attributes("Code").First();
+                                XAttribute tzmMchQuantity = mch.Attributes("Quantity").First();                             
                                 //TzmMch0.Add(new TzmMch() { Caption = Mch1.Value.ToString() });
                                 this.Add(new Chapter()
                                 {
-                                    Chaptername = Chapter1.Value.ToString(),
+                                    Chaptername = chapter1.Value.ToString(),
                                     PositionNumber = Count,
-                                    PositionCode = Code.Value.ToString(),
-                                    Positionname = Caption.Value.ToString(),
+                                    PositionCode = code.Value.ToString(),
+                                    Positionname = caption.Value.ToString(),
                                     PositionUnits = units.Value.ToString(),
-                                    PositionQuantity = Quantity1.Value.ToString(),
+                                    PositionQuantity = result.ToString(),
 
-                                    TzmMchname = Mch1.Value.ToString()
+                                    TzmMchname = mch1.Value.ToString(),
+                                    TzmMchCode = tzmMchCode.Value.ToString(),
+                                    TzmMchQuantity = tzmMchQuantity.Value.ToString()
                                 });
                             }
 
